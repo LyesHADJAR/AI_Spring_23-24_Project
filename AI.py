d0f83e59c82cec03f458507d026f953d56f129fc
Lyes HADJAR <lyes.hadjar@ensia.edu.dz>
2024-05-10 18:40:39 +0100

    Fixed Errors and created main program

--- AI.py
+++ AI.py
@@ -4,22 +4,22 @@ import copy
 class Product:
     def __init__(self, name, production, Strategic, removable, productivity, Season):
         self.name = name #string
-        self.production = production #int
-        self.Strategic = Strategic #boolean
-        self.productivity = productivity #float
-        self.Season = Season #list of strings
-        self.removable = removable #boolean
+        self.production = production # int
+        self.Strategic = Strategic # boolean
+        self.productivity = productivity # float
+        self.Season = Season # list of strings
+        self.removable = removable # boolean
 
 
 class City:
-    def __init__(self, name, agriculture_land, unused_land, land_used, products):
+    def __init__(self, name, unused_land, land_used, products):
         self.name = name #string
         self.unused_land = unused_land # total (int)
         self.land_used = land_used # dictionary ( product : land_used )
         self.products = products # dictionary ( product : Product )
 class Country:
     def __init__(self, cities, consumption , total_production, prices):
-        self.cities = cities #dictionary ( city : City )
+        self.cities = cities # dictionary ( city : City )
         self.consumption = consumption # dictionary ( product : consumption )
         self.total_production = total_production # dictionary ( product : total_production )
         self.prices = prices # dictionary ( product : list of prices each season )
@@ -30,14 +30,13 @@ class Country:
           for value in city.land_used.values():
               total_land_used += value
         return total_land_used
+    
     def getUnusedLand(self):
         total_land_unused = 0
         for city in self.cities.values():
-          for value in city.unused_land.values():
-              total_land_unused += value
+              total_land_unused += city.unused_land
         return total_land_unused
-    def __repr__(self):
-        return f"Country with cities: {[city.name for city in self.cities]}"
+    
 
     def update_production(self, product_name, additional_production):
         for city in self.cities.values():
@@ -107,7 +106,7 @@ class GraphSearch:
             explored.add(node.state)
 
             for action in self.problem.actions(node.state):
-                child_node = self.problem.result(node.state, action)
+                child_node = self.problem.result(node, action)
                 if child_node.state not in explored:
                     frontier.put(child_node, child_node.priority)
 
@@ -134,15 +133,14 @@ class GraphSearch:
         return list(reversed(path))
 
 class AgricultureProblem:
-    def __init__(
-        self, initial_state, Search_method
-    ):  # need to add goal state,transition model,path cost
-        self.initial_state = initial_state
+    def __init__(self, initial_state, Search_method):  # need to add goal state,transition model,path cost
+        self.initial_state = copy.deepcopy(initial_state)
         self.Search_method = Search_method
         number_of_products = len(initial_state.total_production.keys())
         self.counters = []
         for i in range(number_of_products):
             self.counters.append(5) 
+        self.goal_state = self.generate_goal(self.initial_state)
 
     def cost(self, state):
        return state.getTotalLandUsed()
@@ -153,17 +151,18 @@ class AgricultureProblem:
         for product in state.total_production.keys():
           if product=="other":
             continue
-          
-          for i in range(0, len(state.cities)):
+          i=0
+          for value in state.cities.keys():
                 if i == 0:
                     productivity = (
-                        state.cities[i].products[product.name].land_used
-                        / state.cities[i].products[product.name].production
+                        state.cities[value].land_used[product]
+                        / state.cities[value].products[product].production
                     )
+                    i += 1
                 else:
                     temp = (
-                        state.cities[i].products[product.name].land_used
-                        / state.cities[i].products[product.name].production
+                        state.cities[value].land_used[product]
+                        / state.cities[value].products[product].production
                     )
                     if temp < productivity:
                         productivity = temp
@@ -182,7 +181,7 @@ class AgricultureProblem:
       best_value = float('-inf')
       action=None
       for action in self.actions(node.state):
-        child_state = self.result(node.state, action)
+        child_state = self.result(node, action)
         child_value = self.hill_climbing_heuristic(child_state, self.counters)
         if child_value  > best_value:
             best_neighbor = child_state
@@ -191,19 +190,22 @@ class AgricultureProblem:
       return best_neighbor
    
     def result(self, state, action):
-        newState = copy.deepcopy(state)
-        additionalProduction = 10 # the constant to be fixed
-        additionalLand = 10 # the constatn to be fixed
-        
-        newState.total_producton[action[1]] += additionalProduction
-        newState.cities[action[0]].product[action[1]].land_used+=additionalLand
-        newState.cities[action[0]].product[action[1]].production+=additionalProduction
-        newState.cities[action[0]].product[action[1]].productivity=newState.cities[action[0]].product[action[1]].production/newState.cities[action[0]].product[action[1]].land_used
+        newState = copy.deepcopy(state.state)
+        additionalProduction = 1000 # the constant to be fixed
+        additionalLand = 0 # the constatn to be fixed
+        productivity=newState.cities[action[0]].products[action[1]].production/newState.cities[action[0]].land_used[action[1]]
+        additionalLand =additionalProduction/productivity
+        if newState.cities[action[0]].unused_land>additionalLand:
+            additionalLand=newState.cities[action[0]].unused_land
+            additionalProduction=productivity*additionalLand
+    
+        newState.total_production[action[1]] += additionalProduction
+        newState.cities[action[0]].land_used[action[1]]+=additionalLand
+        newState.cities[action[0]].products[action[1]].production+=additionalProduction
+        newState.cities[action[0]].products[action[1]].productivity=newState.cities[action[0]].products[action[1]].production/newState.cities[action[0]].land_used[action[1]]
         newState.cities[action[0]].unused_land=newState.cities[action[0]].unused_land-additionalLand
-        newState.land_used[action[1]] += additionalLand
-        
-        
-        total_land_used = state.get_total_land_used(state) # To be added ez
+        newState.cities[action[0]].land_used[action[1]] += additionalLand
+        #total_land_used = state.getTotalLandUsed(state) # To be added ez
         newNode = Node(newState, state, action, additionalLand, 0)
         newNode.priority = self.As_node_cost(newNode)
         return newNode 
@@ -257,23 +259,24 @@ class AgricultureProblem:
             continue
         else:
             newState.total_production[product] = state.consumption[product] * 1.17
-        return newState
+      return newState
    
-    def generate_goal(self, initial_state ):
+    def generate_goal(self, initial_state):
         new_goal = self.self_sufficiency(initial_state)
         strategic = 0
         non_strategic = 0
-        average_productivity = dict([])
+        average_productivity = {}
         ##we can create a function get productivity that return a dic containing the productivity of a specific product in each wilaya
-        for wilaya in initial_state.wilayas.values():
-            for Product in wilaya.Product.values():
-                if Product.strategic == True:
+        for City in initial_state.cities.values():
+            for Product in City.products.values():
+                average_productivity[Product.name] =0
+                if Product.Strategic == True:
                     strategic += 1
                 else:
                     non_strategic += 1
                 average_productivity[Product.name] = (
                     average_productivity[Product.name]
-                    + Product.production / Product.land_used
+                    + Product.production / City.land_used[Product.name]
                 )  ##to review
 
         for key in average_productivity.keys():
@@ -291,18 +294,51 @@ class AgricultureProblem:
         Additional_production = dict([])
 
         for key in average_productivity.keys():
-            if initial_state.wilayas.Product[key].strategic == True:
-                Additional_production[key] = (
-                    average_productivity[key] * additional_land_strategic
-                )
-            else:
-                Additional_production[key] = (
-                    average_productivity[key] * additional_land_non_strategic
-                )
-
+            for City in initial_state.cities.values():
+                if key in City.products.keys():
+                    if City.products[key].Strategic == True:
+                        Additional_production[key] = (
+                            average_productivity[key] * additional_land_strategic
+                        )
+                    else:
+                        Additional_production[key] = (
+                            average_productivity[key] * additional_land_non_strategic
+                        )
+                    break
+                
         for key in Additional_production.keys():
-            new_goal.update_Production(
-                key, Additional_production[key]
-            )  # needs to be definded ez
-
+            new_goal.update_production(key, Additional_production[key])  # needs to be definded ez
+        for key, value in new_goal.total_production.items():
+            print(key," : ", value)
         return new_goal
+
+def main():
+    # Create some products
+    wheat = Product("Wheat", 100, 200, False, True, ["Spring", "Summer"])
+    corn = Product("Corn", 150, False, True, 300, ["Summer", "Fall"])
+
+    # Create a city with these products
+    city1 = City("City1", 500, {"Wheat" : 1000, "Corn" : 500}, {"Wheat": wheat, "Corn": corn})
+
+    # Create a country with this city
+    country1 = Country({"City1" : city1}, {"Wheat": 1000, "Corn": 1000}, {"Wheat" : 700, "Corn" : 900}, {"Wheat": 2, "Corn": 3})
+
+    # Print the total production
+    print("================================================")
+    print(country1.total_production)
+
+    # Update the total production and print it again
+    print("================================================")
+    country1.update_production("Wheat", 100)
+    print(country1.total_production)
+
+    # Create an AgricultureProblem and find the goal for a given season
+    print("================================================")
+    problem = AgricultureProblem(country1, "UCS")
+    print("================================================")
+    node = Node(country1, None, None, 0, 0)
+    print(problem.result(node, ["City1", "Wheat"]).state.total_production)
+    
+
+if __name__ == "__main__":
+    main()
